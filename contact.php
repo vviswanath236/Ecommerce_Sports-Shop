a5cdb09a5d2e2b3ef215b363147df5e6dd6ca56d
vviswanath236 <150267635+vviswanath236@users.noreply.github.com>
2023-11-08 22:32:54 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,80 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact || SYENERGY Sports Shop</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">SYENERGY Sports Shop</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li><a href="about.php">About</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li class="active"><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+
+
+
+    <div class="row" style="margin-top:30px;">
+      <div class="small-12">
+
+        <p>Wanna get in touch.</p>
+        <p>Email us at: <a href="Mail to: Google.com">support@SYENERGY Solutions.com</a></p>
+         <br>
+        <p> Customer Care Support Tell us at: <a href="#">1800 4562 1000</a></p>
+
+        <footer>
+           <p style="text-align:center; font-size:0.8em;">&copy; SYENERGY Sports Shop. All Rights Reserved.</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
